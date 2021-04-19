f3e74e0938ea72e921e3bd9b9d015fad18c830c1
singh-pk <pksn1111@gmail.com>
2021-04-19 12:18:48 +0530

    Updated API

--- src/services/services.js
+++ src/services/services.js
@@ -4,6 +4,6 @@ const instance = axios.create({
   baseURL: 'https://api.npoint.io',
 });
 
-export const getAppListsApi = () => instance.get('/54d09df281f91e8c146f');
+export const getAppListsApi = () => instance.get('/4ca5aaf459a573940672');
 
 export const getAppStatsApi = () => instance.get(`/d734975d2aee62d197ef`);
